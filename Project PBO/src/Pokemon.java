6dcbca56800f262187c9ff54a168aa17022fdf74
Alvin Bernard Wiyono <alvinbwiyono@gmail.com>
2022-05-08 14:39:11 +0800

    Update Pokemon.java

--- Project PBO/src/Pokemon.java
+++ Project PBO/src/Pokemon.java
@@ -1,3 +1,6 @@
+
+import java.util.Random;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -10,6 +13,17 @@
 public abstract class Pokemon{
     protected int maxHealth,currHealth,damage;
 
+    public Pokemon(int maxHealth, int damage) {
+        this.maxHealth = maxHealth;
+        this.currHealth = maxHealth;
+        this.damage = damage;
+    }
+    
     public abstract int skill();
+
+    public int Attack(){
+        Random rand=new Random();
+        return rand.nextInt(damage)+1;
+    }
     
 }

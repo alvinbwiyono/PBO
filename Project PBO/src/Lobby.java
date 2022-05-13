9b85abeb81f6c30553358120a25f753f68a78c6b
Alvin Bernard Wiyono <alvinbwiyono@gmail.com>
2022-05-13 18:31:52 +0800

    volume

--- Project PBO/src/Lobby.java
+++ Project PBO/src/Lobby.java
@@ -11,6 +11,7 @@ import java.util.logging.Logger;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.sound.sampled.FloatControl;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.ImageIcon;
@@ -81,6 +82,8 @@ public class Lobby extends javax.swing.JFrame {
         } catch (IOException ex) {
             Logger.getLogger(Lobby.class.getName()).log(Level.SEVERE, null, ex);
         }
+        FloatControl volume = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+        volume.setValue(-5f);
     clip.start();
     clip.loop(Clip.LOOP_CONTINUOUSLY);
     }

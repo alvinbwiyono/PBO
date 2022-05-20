f6f9ee82cb81578bd3b9d8c0ff6f48b5f1f190b5
Alvin Bernard Wiyono <alvinbwiyono@gmail.com>
2022-05-20 15:02:12 +0800

    Update arctozolt.java

--- Project PBO/src/arctozolt.java
+++ Project PBO/src/arctozolt.java
@@ -13,9 +13,8 @@ import javax.swing.ImageIcon;
 public class arctozolt extends Pokemon implements Ulti{
     
     public arctozolt() {
-        // Stun
+        // Stun (Skill 2 Ulti 4)
         super(95,5);
-        // 0,0 Sementara mek buat ga error ae
         gambar = new javax.swing.ImageIcon(getClass().getResource("/pokemon_kita/arctozolt.png"));
     }
 
